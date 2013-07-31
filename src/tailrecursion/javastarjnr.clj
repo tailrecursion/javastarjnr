7f6ea913d9ae6ade9e45af8f0377c4ca7e7d21d7
Alan Dipert <alan@dipert.org>
2013-07-31 17:43:23 -0400

    don't actually need javastar!

--- src/tailrecursion/javastarjnr.clj
+++ src/tailrecursion/javastarjnr.clj
@@ -1,16 +1,8 @@
 (ns tailrecursion.javastarjnr
-  (:require [tailrecursion.javastar :refer [compile-java]])
   (:import jnr.ffi.LibraryLoader))
 
-(def interface
-  (compile-java "LibC"
-                "public interface LibC {
-                   int puts(String s);
-                 }"))
-
-(def libc
-  (.load (LibraryLoader/create interface) "c"))
-
 (defn -main [& args]
-  (let [ret (.puts libc "Hello, World")]
-    (println "Return value: " ret)))
+  (-> (gen-interface :name "LibC" :methods [[puts [String] int]])
+      LibraryLoader/create
+      (.load "c")
+      (.puts "Hello, World")))
